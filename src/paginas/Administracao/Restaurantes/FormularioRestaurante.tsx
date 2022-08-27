8527ab9a7be3fdbf1dab6e715fd58c0096c0ee5a
jonas <jonasmirandaa21@gmail.com>
2022-08-27 18:53:41 -0300

    feat: create restaurant register

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,34 @@
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+
+const FormularioRestaurante = () => {
+  const [nomeRestaurante, setNomeRestaurante] = useState("");
+
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    axios
+      .post("http://localhost:8000/api/v2/restaurantes/", {
+        nome: nomeRestaurante,
+      })
+      .then(() => {
+        alert("Restaurante cadastrado com sucesso");
+      });
+  };
+
+  return (
+    <form onSubmit={aoSubmeterForm}>
+      <TextField
+        value={nomeRestaurante}
+        onChange={(evento) => setNomeRestaurante(evento.target.value)}
+        id="standard-basic"
+        label="Nomde do Restaurante"
+        variant="standard"
+      />
+      <Button type="submit" variant="outlined">
+        Outlined
+      </Button>
+    </form>
+  );
+};
+export default FormularioRestaurante;
